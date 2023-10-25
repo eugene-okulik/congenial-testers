a91d125cb5fbeb4c9858bfb693b6f6e42f8bea96
Dmitriy Popov <bzpopovdo@gmail.com>
2023-10-25 22:43:49 +0300

    Создал словарь с ключами и значениями

---
+++ homework/dmitriy_popov/Homework_4/dict_task.py
@@ -0,0 +1,8 @@
+my_dict = {
+    'tuple': '(1, 3, 7, "text", False, 2.42)',
+    'list': '[4, 2, 6, 8, None, "text1", False, 2.42, ]',
+    'dict': '{"one": "value", "two": "value2", "three":"value3", "for": "value4", "five": "value5"}',
+    'set': '{1, 3, 6, 7, None, "text3", False, 2.42, 3, 7}'
+}
+
+print(my_dict['tuple'])
