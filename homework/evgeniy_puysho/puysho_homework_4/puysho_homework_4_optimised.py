0e563f6897b31f6a9aff9b45e3a4624e40db66d4
Evgeniy Puysho <evg.puisho@gmail.com>
2023-10-26 17:07:25 +0300

    optimised code for dictionary

---
+++ homework/evgeniy_puysho/puysho_homework_4/puysho_homework_4_optimised.py
@@ -0,0 +1,30 @@
+my_dict = {
+    'tuple': (1, 2, 3, 4, 5),
+    'list': [1, 'dva', 3.45, None, True],
+    'dict': {'one': 1, 2: 'aga...', 3: 'blin4ik', 'four': 45, 'five': False},
+    'set': {'aboba', 2, 3, 4}
+    }
+
+# добавил элемент в конец списка
+my_dict['list'].append('Noviy element')
+
+# удаление элемента 'dva' из списка
+my_dict['list'].remove('dva')
+
+# добавление нового элемента в словарь
+my_dict['dict']['i am a tuple'] = 'luboe znachenie'
+
+# удаление элемента из словаря
+my_dict['dict'].pop('five')
+
+# добавление элемента во множество
+my_dict['set'].add('ya dobavil')
+
+# удаление элемента из множетсва
+my_dict['set'].remove('aboba')
+
+# распечатывание последнего элемент кортежа
+print(my_dict['tuple'][-1])
+
+# выводим словарь на экран
+print(my_dict)
