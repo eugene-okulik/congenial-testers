f9b7eeb84cf87284256ebad5566613d8adf75076
Alina Kravchenko <coffee.fox@mail.ru>
2023-10-26 11:57:26 +0300

    alina_homework_4

--- homework/AlinaKravch/alina_homework_4/alina_homework_4.py
+++ homework/AlinaKravch/alina_homework_4/alina_homework_4.py
@@ -4,23 +4,11 @@ dictionary = { 'my_tuple':(1, 2.5, 'Alina', 4, True),
             'my_set':{3, None, 'Fine', False, 2.42}}
 
 print(dictionary['my_tuple'][-1])
-
 dictionary['my_list'].append(43)
-# print(dictionary['my_list'])
-
 dictionary['my_list'].pop(2)
-# print(dictionary['my_list'])
-
 dictionary['my_dict']['i am a tuple'] = 'Alex'
-# print(dictionary['my_dict'])
-
 del dictionary['my_dict']['two']
-# print(dictionary['my_dict'])
-
 dictionary['my_set'].add('end')
-# print(dictionary['my_set'])
-
 dictionary['my_set'].remove('end')
-# print(dictionary['my_set'])
 
 print(dictionary)
