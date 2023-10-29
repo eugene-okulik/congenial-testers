b4db3211f144d6ac3f1531ab996eb988f7e3597e
Nurshat Duise <duise.nurshat@gmail.com>
2023-10-29 14:35:47 +0600

    HW 5 done

---
+++ homework/duise_nurshat/HW_5/HW_5_python_basics_3.py
@@ -0,0 +1,26 @@
+
+#task 1
+
+person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+
+name, last_name, city, phone, country = person
+
+text1 = f'{'name, last_name, city, phone, country'}'
+print(text1)
+
+#task 2
+
+list1 = ['результат', 'операции:', 42]
+list2 = ['результат', 'операции:', 54]
+
+numb1 = int(list1[2])
+numb2 = int(list2[2])
+print(numb1 + 10)
+print(numb2 + 10)
+
+#task 3
+
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+print('Students', ', '.join(students), 'study these subjects:', ', '.join(subjects))
