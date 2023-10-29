8caa1401f2a68729b453a33819b7b1768f1c6b3e
Evgenii <teror1716@yandex.ru>
2023-10-29 21:13:54 +0300

    Fixed. Fixed the searching of index. Now it can find any nubmer

--- homework/evgenii_timofeev/homework_5_python/homework_5.py
+++ homework/evgenii_timofeev/homework_5_python/homework_5.py
@@ -5,8 +5,10 @@ name, last_name, city, phone, country = person
 # Task №2
 data_one = "результат операции: 42"
 data_two = "результат операции: 54"
-data_one_index = data_one.index(data_one[-3:])
-data_two_index = data_two.index(data_two[-3:])
+
+data_one_index = data_one.index(data_one.split()[-1])
+data_two_index = data_two.index(data_two.split()[-1])
+
 
 print(
     f"Результат первой строки = {float(data_one[data_one_index:]) + 10} \n"
---
+++ homework/evgenii_timofeev/homework_5_python/test.py
@@ -0,0 +1,2 @@
+students = ["Ivanov", "Petrov", "Sidorov"]
+print(students[::-1])
