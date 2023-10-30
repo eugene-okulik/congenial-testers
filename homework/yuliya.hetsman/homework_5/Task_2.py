96765d5f507d7a92fa099c7ab023991658246fa5
Yuliya Hetsman <yuliya.hetsman@gmail.com>
2023-10-30 18:10:49 +0300

    Added homework 5

---
+++ homework/yuliya.hetsman/homework_5/Task_2.py
@@ -0,0 +1,10 @@
+res_1 = 'результат операции: 42'
+res_2 = 'результат операции: 54'
+
+num_1 = res_1.index(res_1[-2:])
+sum1 = num_1 + 10
+print(sum1)
+
+num_2 = res_2.index(res_2[-2:])
+sum2 = num_2 + 10
+print(sum2)
