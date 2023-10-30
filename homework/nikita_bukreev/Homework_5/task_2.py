b3e30bf66e19f30f02e078ae320e201ad6daecba
NikitaBukreev <byka-93@mail.ru>
2023-10-30 19:39:53 +0300

    fix_task_2_lint

--- homework/nikita_bukreev/Homework_5/task_2.py
+++ homework/nikita_bukreev/Homework_5/task_2.py
@@ -5,8 +5,8 @@ string_2 = 'Результат ОПЕРАЦИИ: 53453454'
 
 replace_str_1 = string_1.replace(' ', '')
 index_str_1 = replace_str_1.index(':')
-print(int(replace_str_1[index_str_1:].replace(':', '')) + 10 )
+print(int(replace_str_1[index_str_1:].replace(':', '')) + 10)
 
 replace_str_2 = string_2.replace(' ', '')
 index_str_2 = replace_str_2.index(':')
-print(int(replace_str_2[index_str_2:].replace(':', '')) + 10 )
+print(int(replace_str_2[index_str_2:].replace(':', '')) + 10)
