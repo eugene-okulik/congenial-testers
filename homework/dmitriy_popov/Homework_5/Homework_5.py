fe313a866a6a9d4d7e4ad49a23679f764240d9cf
Dmitriy Popov <bzpopovdo@gmail.com>
2023-10-31 21:17:33 +0300

    Homework_5 fix Task2

--- homework/dmitriy_popov/Homework_5/Homework_5.py
+++ homework/dmitriy_popov/Homework_5/Homework_5.py
@@ -4,26 +4,19 @@ name, last_name, city, phone, country = person
 
 
 # Task 2
-result_1 = 'результат операции: 42'
-result_2 = 'результат операции: 54'
+res_1 = 'результат операции: 42'
+res_2 = 'результат операции: 54'
 
-number_1 = int(result_1[-2:])
-sum_1 = number_1 + 10
-print(f'Результат сложения: {sum_1}')
+res_1_index = res_1.index(res_1[20:])
+res_2_index = res_2.index(res_2[20:])
 
-number_2 = int(result_2[-2:])
-sum_2 = number_2 + 10
-print(f'Результат сложения: {sum_2}')
+print(
+    f'Результат первого сложения: {int(res_1[res_1_index:]) + 10} \n'
+    f'Результат второго сложения: {int(res_2[res_1_index:]) + 10}'
+)
 
 
-# Task 3 Variant 1
-students = ['Ivanov', 'Petrov', 'Sidorov']
-st_1, st_2, st_3 = students
-subjects = ['math', 'biology', 'geography']
-sub_1, sub_2, sub_3 = subjects
-print(f'Students {st_1}, {st_2}, {st_3} study these subjects: {sub_1}, {sub_2}, {sub_3}')
-
-# Task 3 Variant 2
+# Task 3
 students_1 = ['Ivanov', 'Petrov', 'Sidorov']
 subjects_2 = ['math', 'biology', 'geography']
 print(f'Students {", ".join(students_1)} study these subjects: {", ".join(subjects_2)}')
