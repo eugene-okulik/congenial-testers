e2168df63bd09c881064884442be47c65fe9859f
MaksimBuhuk <aomine194@yandex.ru>
2023-11-01 20:33:05 +0300

    Revert "maksimb_fix_HW_done"
    This reverts commit 7064bdff8e94ef68a8147024f85d0b36fe8059e1.

--- homework/maksim_boguk/lesson5/python_basic3.py
+++ homework/maksim_boguk/lesson5/python_basic3.py
@@ -15,6 +15,4 @@ print(
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 
-print(
-    f'Students {", ".join(students)} study these subjects: {", ".join(subjects)}'
-)
+print(f'Students {", ".join(students)} study these subjects: {", ".join(subjects)}')
