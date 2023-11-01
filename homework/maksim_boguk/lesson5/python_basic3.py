bc82133294383402ed2b3e328d412634019e201a
MaksimBuhuk <aomine194@yandex.ru>
2023-11-01 20:25:59 +0300

    maksimb_fixHW5

--- homework/maksim_boguk/lesson5/python_basic3.py
+++ homework/maksim_boguk/lesson5/python_basic3.py
@@ -3,13 +3,16 @@ name, last_name, city, phone, country = person
 
 a = "результат операции: 42"
 b = "результат операции: 54"
+
 suma = a.index(a.split()[-1])
 sumb = b.index(b.split()[-1])
 
-print(f"Первый результат операции = {int(a[suma:]) + 10} \n"
-      f"Второй результат операции = {int(b[sumb:]) + 10} \n"
-      )
+print(
+    f"Первый результат операции = {int(a[suma:]) + 10} \n"
+    f"Второй результат операции = {int(b[sumb:]) + 10} \n"
+)
 
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
+
 print(f'Students {", ".join(students)} study these subjects: {", ".join(subjects)}')
