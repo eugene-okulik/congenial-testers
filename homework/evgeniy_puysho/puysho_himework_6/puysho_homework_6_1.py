f5a07c25cddb7146844389095e7e1ad120f12e68
Evgeniy Puysho <evg.puisho@gmail.com>
2023-11-01 18:37:53 +0300

    optimized code

--- homework/evgeniy_puysho/puysho_himework_6/puysho_homework_6_1.py
+++ homework/evgeniy_puysho/puysho_himework_6/puysho_homework_6_1.py
@@ -3,10 +3,15 @@ my_text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '\
 words = my_text.split()
 new_text = []
 for word in words:
+    '''
+    вот так вот я изначально сделал, но потом переписал
+    if word.endswith('.'):
+    word = word.replace('.', 'ing') + '.'
     if word.endswith(','):
-        word = word.replace(',', 'ing') + ','
-    elif word.endswith('.'):
-        word = word.replace('.', 'ing') + '.'
+    word = word.replace('.', 'ing') + ','
+    '''
+    if word.endswith((',', '.')):
+        word = word.replace(word[-1], 'ing') + word[-1]
     else:
         word = word + 'ing'
     new_text.append(word)
