983cbfbd3214eea0658cefde967ccec1dd50a22d
Nurshat Duise <duise.nurshat@gmail.com>
2023-11-05 18:36:50 +0600

    Добавила вторую задачу в HW 6

---
+++ homework/duise_nurshat/HW_6/Ex_1.py
@@ -0,0 +1,8 @@
+text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, '
+        'dignissim vitae libero')
+
+new_text = text.split()
+print(new_text)
+
+for word in new_text:
+    print(word + 'ing')
