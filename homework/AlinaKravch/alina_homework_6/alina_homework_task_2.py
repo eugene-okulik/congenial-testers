5b723241f47a9b5b9a5b94ec7c5ff2159b132f61
Alina Kravchenko <coffee.fox@mail.ru>
2023-11-05 10:48:27 +0300

    alina_homework_6_task_2

---
+++ homework/AlinaKravch/alina_homework_6/alina_homework_task_2.py
@@ -0,0 +1,13 @@
+# Write a program that tries the sequence from 1 to 100. For numbers divisible by 3 it should write:
+# "Fuzz" instead of printing the number, and for numbers divisible by 5 it should print "Buzz".
+# For numbers that are multiples of both 3 and 5, it should print "FuzzBuzz". Otherwise, print the number.
+
+for n in range(1, 101):
+    if n % 3 == 0:
+        print('Fuzz')
+    elif n % 5 == 0:
+        print('Buzz')
+    elif (n % 3 == 0) and (n % 5 == 0):
+        print('FuzzBuzz')
+    else:
+        print(n)
