b366f4b1f520237f590eff7ab8e17a7ecfb70750
Evgeniy Puysho <evg.puisho@gmail.com>
2023-11-06 14:45:39 +0300

    fixed lint errors

--- homework/evgeniy_puysho/puysho_homework_7/puysho_hm_7_3.py
+++ homework/evgeniy_puysho/puysho_homework_7/puysho_hm_7_3.py
@@ -1,14 +1,14 @@
 def calc(stroka):
     results = stroka.split(' ')[-1]
-    res = int(results) + 10
+    results = int(results) + 10
     print(f'Результат сложения: {results}')
 
 
 stroka = ['результат операции: 42',
-           'результат операции: 54',
-           'результат работы программы: 209',
-           'результат: 2'
-           ]
+          'результат операции: 54',
+          'результат работы программы: 209',
+          'результат: 2'
+          ]
 
 for results in stroka:
     calc(results)
