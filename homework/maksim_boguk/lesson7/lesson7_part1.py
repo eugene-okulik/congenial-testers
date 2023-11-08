44abef9123a709a2a1839ca3e419807704d516af
MaksimBuhuk <aomine194@yandex.ru>
2023-11-08 20:25:24 +0300

    maksimb_HW7_done

---
+++ homework/maksim_boguk/lesson7/lesson7_part1.py
@@ -0,0 +1,10 @@
+# верное число 3
+while True:
+    u_input = input('Enter number: ')
+    if u_input == 'exit':
+        break
+    if u_input == '3':
+        print('Good job')
+        break
+    else:
+        print('Try again')
