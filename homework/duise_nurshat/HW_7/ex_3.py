b566f055eb0c546d2021169fc80bb96b8957c207
Nurshat Duise <duise.nurshat@gmail.com>
2023-11-12 17:21:12 +0600

    Исправила ошибки указанные в Lint

--- homework/duise_nurshat/HW_7/ex_3.py
+++ homework/duise_nurshat/HW_7/ex_3.py
@@ -3,8 +3,10 @@ text2 = "результат операции: 54"
 text3 = "результат работы программы: 209"
 text4 = "результат: 2"
 
+
 def func(text, numb):
     srez = int(text[text.index(numb):])
     print(srez + 10)
+
 
 func(text3, '209')
