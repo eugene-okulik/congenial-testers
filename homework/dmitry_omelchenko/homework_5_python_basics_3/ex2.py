25dad5a102d7ec583b0b6711f567e29c3a168ddb
fixplays <dmitriyomeltschenko@gmail.com>
2023-11-12 23:48:49 +0100

    changes

--- homework/dmitry_omelchenko/homework_5_python_basics_3/ex2.py
+++ homework/dmitry_omelchenko/homework_5_python_basics_3/ex2.py
@@ -3,5 +3,5 @@ result1 = "operation result: 42"
 result2 = "operation result: 54"
 
 # Извлечение чисел, прибавление 10  и печать результатов
-print("Result 1:", int(result1.split()[-1]) + 10)
-print("Result 2:", int(result2.split()[-1]) + 10)
+print("Result 1:", int(result1[result1.index(':') + 1:]) + 10)
+print("Result 2:", int(result2[result2.index(':') + 1:]) + 10)
