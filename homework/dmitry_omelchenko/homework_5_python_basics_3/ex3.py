dbd98badb380c333f5ede8527c923501f223ef8e
fixplays <dmitriyomeltschenko@gmail.com>
2023-11-12 22:49:46 +0100

    done_with_homework_5

---
+++ homework/dmitry_omelchenko/homework_5_python_basics_3/ex3.py
@@ -0,0 +1,6 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+result_text = f"Students {', '.join(students)} study these subjects: {', '.join(subjects)}."
+print(result_text)
+
