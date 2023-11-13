1fdbf313ead4f4ad80587d6264abf81b171053a1
MaksimBuhuk <aomine194@yandex.ru>
2023-11-13 18:54:15 +0300

    maksimb_lesson9_done

---
+++ homework/maksim_boguk/lesson9/lesson9_part1.py
@@ -0,0 +1,7 @@
+import datetime
+
+
+time_now = datetime.datetime.strptime("01/15/2023 12:05:33", "%m/%d/%Y %H:%M:%S")
+print('Текущий месяц:', time_now.month)
+print(time_now.strftime('%d.%m.%y, %H:%M'))
+
