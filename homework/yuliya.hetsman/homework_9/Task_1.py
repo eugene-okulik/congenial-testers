1c0ec47f91a48979615512f96f094066104aca7b
Yuliya Hetsman <yuliya.hetsman@gmail.com>
2023-11-16 17:03:06 +0300

    homework for lesson 9

---
+++ homework/yuliya.hetsman/homework_9/Task_1.py
@@ -0,0 +1,6 @@
+import datetime
+
+example_date = 'Jan 15, 2023 - 12:05:33'
+python_date = datetime.datetime.strptime(example_date, '%b %d, %Y - %X')
+print(python_date)
+print(python_date.strftime('%d.%m.%Y, %H:%M'))
