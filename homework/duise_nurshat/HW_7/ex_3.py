f5cd392861ce480a9530542a9d7de3518af8f2cc
Nurshat Duise <duise.nurshat@gmail.com>
2023-11-19 16:35:35 +0600

    временно

--- homework/duise_nurshat/HW_7/ex_3.py
+++ homework/duise_nurshat/HW_7/ex_3.py
@@ -1,12 +1,22 @@
-text1 = "результат операции: 42"
-text2 = "результат операции: 54"
-text3 = "результат работы программы: 209"
-text4 = "результат: 2"
+import re
+
+def add_ten_to_numbers(text):
+    numbers = re.findall(r'\d+', text)
+    modified_numbers = [int(number) + 10 for number in numbers]
+    return modified_numbers
 
 
-def func(text, numb):
-    srez = int(text[text.index(numb):])
-    print(srez + 10)
+text1 = int(input('результат операции: '))
+text2 = int(input('результат операции: '))
+text3 = int(input('результат работы программы: '))
+text4 = int(input('результат: '))
 
+# Получаем числа из каждой строки и добавляем к ним 10
+numbers_text1 = add_ten_to_numbers(text1)
+numbers_text2 = add_ten_to_numbers(text2)
+numbers_text3 = add_ten_to_numbers(text3)
 
-func(text3, '209')
+# Вывод измененных чисел
+print("Числа из первой строки + 10:", numbers_text1)
+print("Числа из второй строки + 10:", numbers_text2)
+print("Числа из третьей строки + 10:", numbers_text3)
