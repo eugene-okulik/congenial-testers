7e0c9cc0f776bd2539c5430c2395a3b35c697e82
Nurshat Duise <duise.nurshat@gmail.com>
2023-11-19 16:46:25 +0600

    Сделано 1 упр

--- homework/duise_nurshat/HW_6/Ex_1.py
+++ homework/duise_nurshat/HW_6/Ex_1.py
@@ -1,11 +1,15 @@
 text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, '
         'dignissim vitae libero')
 
-rep = text.replace(',', '').replace('.', '')
-words = rep.split()
+words = text.split()
+
 new_text = []
 for word in words:
-    new_text.append(word + 'ing')
+    if word.endswith((',', '.')):
+        word = word.replace(word[-1], 'ing') + word[-1]
+    else:
+        word = word + 'ing'
+    new_text.append(word)
 
 result = " ".join(new_text)
 print(result)
