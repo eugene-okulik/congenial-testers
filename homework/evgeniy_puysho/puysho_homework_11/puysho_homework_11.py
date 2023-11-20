8bc202218e593821fe83314bd6cbd946e95a7a14
Evgeniy Puysho <evg.puisho@gmail.com>
2023-11-20 13:52:02 +0300

    optimised code

--- homework/evgeniy_puysho/puysho_homework_11/puysho_homework_11.py
+++ homework/evgeniy_puysho/puysho_homework_11/puysho_homework_11.py
@@ -2,12 +2,28 @@ class Books:
     page_material = 'Бумага'
     is_containing_text = True
 
-    def __init__(self, book_name, author, pages_count, isbn_num, is_reserved):
+    def __init__(self, book_name, author, pages_count, isbn_num, is_reserved, subject=None, grade=None):
         self.book_name = book_name
         self.author = author
         self.pages_count = pages_count
         self.isbn_num = isbn_num
         self.is_reserved = is_reserved
+        self.subject = subject
+        self.grade = grade
+
+    def display_info(self):
+        reserved_status = ', зарезевирована' if self.is_reserved else ''
+        additional_info = (f', Предмет: {self.subject}, Класс: {self.grade}' if self.subject and self.grade else '')
+        print(f'Название: {self.book_name}, Автор: {self.author}, Страниц: {self.pages_count}, '
+              f'Материал: {self.page_material}{additional_info}{reserved_status}'
+              )
+
+
+class Schoolbook(Books):
+
+    def __init__(self, book_name, author, pages_count, isbn_num, is_reserved, subject, grade, task):
+        super().__init__(book_name, author, pages_count, isbn_num, is_reserved, subject, grade)
+        self.task = task
 
 
 knigi = [
@@ -18,38 +34,13 @@ knigi = [
     Books('Забавная Приключение', 'Анна Петрова', 99, 7321, True)
 ]
 
-for book in knigi:
-    if book.is_reserved:
-        print(f"Название: {book.book_name}, Автор: {book.author}, cтраниц: {book.pages_count}, "
-              f"материал: {book.page_material}, зарезервирована"
-              )
-    else:
-        print(f"Название: {book.book_name}, Автор: {book.author}, cтраниц: {book.pages_count}, "
-              f"материал: {book.page_material}"
-              )
-
-
-class Schoolbook(Books):
-
-    def __init__(self, book_name, author, pages_count, isbn_num, is_reserved, subject, grade, task):
-        super().__init__(book_name, author, pages_count, isbn_num, is_reserved)
-        self.subject = subject
-        self.grade = grade
-        self.task = task
-
-
 school_books = [
     Schoolbook('Алгебра 8-й класс', 'Евклид', 222, 9876, False, 'Математика', 8, True),
     Schoolbook('Писатели 20-го века', 'Абоба', 222, 9876, False, 'Русская литература', 5, True),
-    Schoolbook('Оптика', 'Автор', 222, 9876, False, 'Физика', 11, True),
+    Schoolbook('Оптика', 'Кто-то', 222, 9876, False, 'Физика', 11, True),
     Schoolbook('Спина болеть не будет', 'А. Невский', 123, 9876, True, 'Физкультура', 7, True),
 ]
+for book in knigi:
+    book.display_info()
 for school_book in school_books:
-    if school_book.is_reserved:
-        print(f"Название: {school_book.book_name}, Автор: {school_book.author}, cтраниц: {school_book.pages_count}, "
-              f"предмет: {school_book.subject}, класс: {school_book.grade}, зарезервирована"
-              )
-    else:
-        print(f"Название: {school_book.book_name}, Автор: {school_book.author}, cтраниц: {school_book.pages_count}, "
-              f"предмет: {school_book.subject}, класс: {school_book.grade}"
-              )
+    school_book.display_info()
