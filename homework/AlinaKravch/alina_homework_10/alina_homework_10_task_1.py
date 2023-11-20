78200ece0f7ed1675bcf981bfb4f3bbc7c710048
Alina Kravchenko <coffee.fox@mail.ru>
2023-11-20 10:57:23 +0300

    alina_homework_10

---
+++ homework/AlinaKravch/alina_homework_10/alina_homework_10_task_1.py
@@ -0,0 +1,30 @@
+def calc(func):
+    def wrapper(num_1, num_2):
+        if num_1 == num_2:
+            operator = "+"
+        elif num_1 > num_2:
+            operator = "-"
+        elif num_1 < 0 or num_2 < 0:
+            operator = "*"
+        elif num_2 > num_1:
+            operator = "/"
+
+        return func(num_1, num_2, operator)
+
+    return wrapper
+
+
+@calc
+def ass(first, second, operation):
+    if operation == '+':
+        return first + second
+    elif operation == '-':
+        return second - first
+    elif operation == '/':
+        return first / second
+    elif operation == '*':
+        return first * second
+
+
+numberOne = int(input("Enter the first number: "))
+numberTwo = int(input("Enter the second: "))
