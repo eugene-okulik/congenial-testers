4ee5a6a4d5de5752f501394aac5dbce2bef75a1f
Dmitry <62031209+fixplays@users.noreply.github.com>
2023-11-21 19:50:59 +0300

    Delete homework/dmitry_omelchenko/homework_5 directory

--- homework/dmitry_omelchenko/homework_5/ex3.py
+++
@@ -1,5 +0,0 @@
-students = ['Ivanov', 'Petrov', 'Sidorov']
-subjects = ['math', 'biology', 'geography']
-
-result_text = f"Students {', '.join(students)} study these subjects: {', '.join(subjects)}."
-print(result_text)
