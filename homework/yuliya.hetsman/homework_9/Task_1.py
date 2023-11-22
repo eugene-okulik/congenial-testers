3706edcc2e1d6efe74e400ee88bf3a89901a0806
Yuliya Hetsman <yuliya.hetsman@gmail.com>
2023-11-22 15:14:49 +0300

    fixed second print in task 1

--- homework/yuliya.hetsman/homework_9/Task_1.py
+++ homework/yuliya.hetsman/homework_9/Task_1.py
@@ -2,5 +2,7 @@ import datetime
 
 example_date = 'Jan 15, 2023 - 12:05:33'
 python_date = datetime.datetime.strptime(example_date, '%b %d, %Y - %X')
-print(python_date)
+full_name = python_date.strftime('%B')
+
+print(full_name)
 print(python_date.strftime('%d.%m.%Y, %H:%M'))
