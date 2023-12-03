373dd4858eafecf6fbe6a80ee031b50d264ea94e
Dmitriy Popov <bzpopovdo@gmail.com>
2023-12-03 19:18:51 +0300

    Homework_13 done. refactor print

--- homework/dmitriy_popov/Homework_13/Task_1.py
+++ homework/dmitriy_popov/Homework_13/Task_1.py
@@ -20,12 +20,12 @@ for data_line in read_file(okulik_file_path):
     python_date = datetime.datetime.strptime(date_in_line, '%Y-%m-%d %H:%M:%S.%f')
 
     if number_in_line == '1':
-        print(python_date + datetime.timedelta(weeks=1))
+        print(f'Через неделю от даты: "{date_in_line}" будет {python_date + datetime.timedelta(weeks=1)}')
     elif number_in_line == '2':
-        print(python_date.strftime('%A'))
+        print(f'День недели у даты: "{date_in_line}" будет - {python_date.strftime('%A')}')
     else:
         now = datetime.datetime.now()
-        print((datetime.datetime.now() - python_date).days)
+        print(f'Дата: "{date_in_line}" была {(datetime.datetime.now() - python_date).days} дней назад')
 
 
 read_file(okulik_file_path)
