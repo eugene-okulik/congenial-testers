af910dc6a1afad9e3fa4ca36cf90ac9d9c93244f
Eugeny Okulik <tiburon854@gmail.com>
2024-01-10 20:22:53 +0300

    program_examples

--- homework/eugeny_okulik/test.py
+++ homework/eugeny_okulik/test.py
@@ -1,5 +1,9 @@
 result = [{'12.01.2023': 'Error message'}, {'12.01.2023': 'Warning message'}]
-result = {'12.01.2023 13:00': '12.01.2023 13:00 Error message', '12.01.2023 13:01': '12.01.2023 13:01 Warning message', '12.01.2023 13:02': '12.01.2023 13:02 Error message appeared'}
+result = {
+    '12.01.2023 13:00': '12.01.2023 13:00 Error message',
+    '12.01.2023 13:01': '12.01.2023 13:01 Warning message',
+    '12.01.2023 13:02': '12.01.2023 13:02 Error message appeared'
+    }
 
 text = 'Error'
 unwanted = 'appeared'
@@ -14,7 +18,7 @@ result = list(result.values())
 
 result = list(filter(lambda x: unwanted not in x, result))
 
-for res  in result:
+for res in result:
     print(res)
     print(res.index(text))
     print(res[res.index(text):res.index(text) + 10])
