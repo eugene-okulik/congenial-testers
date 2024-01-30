673521c1882893c29c5663894f5e65a76e1fd0e6
Evgeniy Puysho <evg.puisho@gmail.com>
2024-01-31 00:20:23 +0300

    again

--- homework/evgeniy_puysho/puysho_homework_21/api_optimised(probably).py
+++ homework/evgeniy_puysho/puysho_homework_21/api_optimised(probably).py
@@ -27,34 +27,37 @@ UPD_PATCH_PAYLOAD = {"name": "Updated with patch"}
 
 def add_object(payload, url):
     headers = headers_req
-    response = requests.post(BASE_URL, json=payload, headers=headers).json()
+    response = requests.post(url, json=payload, headers=headers).json()
     print(f'Object created:\n {response}')
     new_object = response['id']
-    assert requests.get(f'{url}/{new_object}').status_code == 200, 'Error: object has not been added'
+    full_url = f'{url}/{new_object}'
+    assert requests.get(full_url).status_code == 200, 'Error: object has not been added'
     return response['id']
 
 
 def update_object(obj_id, payload, url):
     headers = headers_req
-    response = requests.put(f'{url}/{obj_id}', json=payload, headers=headers).json()
+    full_url = f'{url}/{obj_id}'
+    response = requests.put(full_url, json=payload, headers=headers).json()
     print(f'Object updated:\n {response}')
     assert response['name'] == 'UPD: Apple MacBook Pro 16', 'Error: check request body'
-    assert requests.get(f'{url}/{obj_id}').status_code == 200, 'Error: object does not exist'
+    assert requests.get(full_url).status_code == 200, 'Error: object does not exist'
 
 
 def update_object_partly(obj_id, payload, url):
     headers = headers_req
-    response = requests.patch(
-        f'{url}/{obj_id}', json=payload, headers=headers).json()
+    full_url = f'{url}/{obj_id}'
+    response = requests.patch(full_url, json=payload, headers=headers).json()
     print(f'Object updated partly:\n {response}')
     assert response['name'] == 'Updated with patch', 'Error: check request body'
-    assert requests.get(f'{url}/{obj_id}').status_code == 200, 'Error: object does not exist'
+    assert requests.get(full_url).status_code == 200, 'Error: object does not exist'
 
 
 def delete_object(obj_id, url):
-    response = requests.delete(f'{url}/{obj_id}')
+    full_url = f'{url}/{obj_id}'
+    response = requests.delete(full_url)
     print(f'Status code: {response.status_code}')
-    assert requests.get(f'{url}/{obj_id}').status_code == 404, 'Object has not been deleted'
+    assert requests.get(full_url).status_code == 404, 'Object has not been deleted'
 
 
 added_object = add_object(ADD_PAYLOAD, BASE_URL)
