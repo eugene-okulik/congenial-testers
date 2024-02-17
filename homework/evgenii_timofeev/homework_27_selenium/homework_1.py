08406737bebe6ca6682ada200400804d7060e3fb
Evgenii <teror1716@yandex.ru>
2024-02-17 16:51:30 +0300

    Fix. fixed according to our task

--- homework/evgenii_timofeev/homework_27_selenium/homework_1.py
+++ homework/evgenii_timofeev/homework_27_selenium/homework_1.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 TEXT = "monkey"
 
@@ -13,7 +12,7 @@ driver.get("https://www.qa-practice.com/elements/input/simple")
 
 search_line = driver.find_element(By.ID, "id_text_string")
 search_line.send_keys(TEXT)
-search_line.send_keys(Keys.ENTER)
+search_line.submit()
 
 result_element = driver.find_element(By.ID, "result-text")
 print(result_element.text)
