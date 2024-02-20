0018f961065c8f78d08f205af12c70c700f3e0be
NikitaBukreev <byka-93@mail.ru>
2024-02-20 19:04:36 +0300

    nikita_bukreev_homework_29_done

---
+++ homework/nikita_bukreev/Homework_29/task_2.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+# Попробовал явные и неявные ожидания, решил оставить в коде для себя, но неявные закоментил=)
+driver = webdriver.Chrome()
+# driver.implicitly_wait(10)
+driver.get('https://the-internet.herokuapp.com/dynamic_loading/2')
+button_start = driver.find_element(By.XPATH, "//button[text()='Start']")
+button_start.click()
+search_text = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'finish')))
+# search_text = driver.find_element(By.ID, 'finish')
+assert search_text.text == 'Hello World!'
